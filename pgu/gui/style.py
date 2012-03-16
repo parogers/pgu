51d8be4346fb884601d545176e215a22d8cb1035
peter.rogers <peter.rogers@2cebf150-ffc6-11dd-8259-6defae454439>
2012-03-16 03:19:51 +0000

    -converted themes to using the new style.ini (rather than config.txt) -added Theme.configure function for further customizing styles -added Theme.putstyle -added convertconfig.py script for converting config.txt files into the new format

--- pgu/gui/style.py
+++ pgu/gui/style.py
@@ -4,49 +4,80 @@
 from . import pguglobals
 from .errors import StyleError
 
-class Style:
-    """The class used by widget for the widget.style
-    
-    This object is used mainly as a dictionary, accessed via widget.style.attr, 
-    as opposed to widget.style['attr'].  It automatically grabs information 
-    from the theme via value = theme.get(widget.cls,widget.pcls,attr)
+class Style(object):
+    """Widget style information. If the style attribute is not explicitly defined in the
+    style, the fallback is to use the default defined by the global theme. Example:
+
+    Arguments:
+        widget -- The widget this style applies to
+        values -- A dictionary of style information to use instead of the theme defaults
+
+    Example:
+        # Create a button
+        w = Button("Testing")
+
+        # Print out the default value for 'padding_left'
+        print w.style.padding_left
+
+        # Change the default style for all buttons
+        app.theme.putstyle("button", "", "padding_left", 10)
+        print w.style.padding_left
+
+        # Define 'padding_left' only for this widget
+        w.style.padding_left = 1
+        # Alternate syntax
+        w.style["padding_left"] = 2
 
     """
-    def __init__(self, obj, dict):
-        self.obj = obj
-        for k,v in dict.items(): self.__dict__[k]=v
+    def __init__(self, widget, values):
+        self.widget = widget
+        for (key, value) in values.items():
+            setattr(self, key, value)
 
     # Verify that the given style is defined, otherwise raises an StyleError exception. This
     # is used by various widgets to check that they have all required style information.
     def check(self, attr):
         if (not self.exists(attr)):
-            desc = self.obj.cls
-            if (self.obj.pcls): desc += "."+self.obj.pcls
+            desc = self.widget.cls
+            if (self.widget.pcls): desc += "."+self.widget.pcls
             raise StyleError("Cannot find the style attribute '%s' for '%s'" % (attr, desc))
 
     # Checks if the given style attribute exists
     def exists(self, attr):
         try:
-            value = pguglobals.app.theme.getstyle(self.obj.cls, self.obj.pcls, attr)
+            self.getstyle(attr)
             return True
         except StyleError:
             return False
 
+    def getstyle(self, attr):
+        if (hasattr(self.widget, "name") and self.widget.name):
+            cls = self.widget.cls
+            pcls = self.widget.pcls
+            if (pcls): pcls += "#" + self.widget.name
+            else: cls += "#" + self.widget.name
+            try:
+                return pguglobals.app.theme.getstyle(cls, pcls, attr)
+            except StyleError:
+                pass
+        return pguglobals.app.theme.getstyle(self.widget.cls, self.widget.pcls, attr)
+
     def __getattr__(self, attr):
         # Lookup the attribute
         try:
-            value = pguglobals.app.theme.getstyle(self.obj.cls, self.obj.pcls, attr)
+            value = self.getstyle(attr)
         except StyleError:
             value = 0
 
-        if attr in (
-            'border_top','border_right','border_bottom','border_left',
-            'padding_top','padding_right','padding_bottom','padding_left',
-            'margin_top','margin_right','margin_bottom','margin_left',
-            'align','valign','width','height',
-            ): self.__dict__[attr] = value
+#        if attr in (
+#            'border_top','border_right','border_bottom','border_left',
+#            'padding_top','padding_right','padding_bottom','padding_left',
+#            'margin_top','margin_right','margin_bottom','margin_left',
+#            'align','valign','width','height',
+#            ): self.__dict__[attr] = value
         return value
 
-    def __setattr__(self, attr, value):
-        self.__dict__[attr] = value
+#    def __setattr__(self, attr, value):
+#        self.__dict__[attr] = value
+
 
